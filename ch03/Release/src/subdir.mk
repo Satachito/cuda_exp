ade9e6654cd9e7536fa0c52bfbb918fbcbfe59ac
sat <satoru.ogura@me.com>
2019-09-25 13:16:58 +0900

    INIT

---
+++ ch03/Release/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../src/NN3.cu 
+
+OBJS += \
+./src/NN3.o 
+
+CU_DEPS += \
+./src/NN3.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-10.1/bin/nvcc -O3 -gencode arch=compute_35,code=sm_35  -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-10.1/bin/nvcc -O3 --compile --relocatable-device-code=false -gencode arch=compute_35,code=compute_35 -gencode arch=compute_35,code=sm_35  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
